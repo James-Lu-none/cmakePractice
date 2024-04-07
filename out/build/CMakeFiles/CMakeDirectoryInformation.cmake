3bd51e82dee2f5a824203ef61967f8b8cb32577b
James-Lu-none <jamesabcde277@gmail.com>
2024-04-07 12:22:53 +0800

    rename build dir

--- out/build/CMakeFiles/CMakeDirectoryInformation.cmake
+++ out/build/CMakeFiles/CMakeDirectoryInformation.cmake
@@ -3,7 +3,7 @@
 
 # Relative path conversion top directories.
 set(CMAKE_RELATIVE_PATH_TOP_SOURCE "/home/james/projects/cProject/cmakePractice")
-set(CMAKE_RELATIVE_PATH_TOP_BINARY "/home/james/projects/cProject/cmakePractice/out/blankBuild")
+set(CMAKE_RELATIVE_PATH_TOP_BINARY "/home/james/projects/cProject/cmakePractice/out/build")
 
 # Force unix paths in dependencies.
 set(CMAKE_FORCE_UNIX_PATHS 1)
--- out/build/cmake_install.cmake
+++ out/build/cmake_install.cmake
@@ -50,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/james/projects/cProject/cmakePractice/out/blankBuild/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/james/projects/cProject/cmakePractice/out/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
