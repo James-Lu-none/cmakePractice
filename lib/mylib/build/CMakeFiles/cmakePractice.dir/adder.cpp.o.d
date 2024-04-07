d9dc4426ce74faf268fa16f81ea01716290ac54f
James-Lu-none <jamesabcde277@gmail.com>
2024-04-07 17:42:25 +0800

    cmake and make

---
+++ lib/mylib/build/CMakeFiles/cmakePractice.dir/adder.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/cmakePractice.dir/adder.cpp.o: \
+ /home/james/projects/cProject/cmakePractice/lib/mylib/adder.cpp \
+ /usr/include/stdc-predef.h \
+ /home/james/projects/cProject/cmakePractice/lib/mylib/adder.h
