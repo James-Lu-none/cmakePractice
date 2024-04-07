23bbd86c15faad2bd96ce91839f3b18424fc1981
James-Lu-none <jamesabcde277@gmail.com>
2024-04-07 18:15:47 +0800

    build with error: no jamesMath

---
+++ src/test/build/CMakeFiles/test.dir/main.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/test.dir/main.cpp.o: \
+ /home/james/projects/cProject/cmakePractice/src/test/main.cpp \
+ /usr/include/stdc-predef.h \
+ /home/james/projects/cProject/cmakePractice/src/test/../lib/mylib/adder.h
