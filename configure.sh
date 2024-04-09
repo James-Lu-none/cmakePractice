40f0848893d9eb16a033c23719db38202d01c95e
James-Lu-none <jamesabcde277@gmail.com>
2024-04-09 14:39:33 +0800

    add configure desc

--- configure.sh
+++ configure.sh
@@ -1,3 +1,8 @@
 #! /bin/sh
 
 cmake -S . -B build
+
+# if we want to modify the options for modules in configure time, we can use -D in cmake
+# for example:
+# `cmake -DGLFW_BUILD_DOCS=OFF -S . -B build`
+# in this case we make GLFW_BUILD_DOCS off to not build the glfw documentation.
