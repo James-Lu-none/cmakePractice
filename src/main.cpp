7573fc024bceed9f4d299080e8f4f1bd2f3be19d
James-Lu-none <jamesabcde277@gmail.com>
2024-04-09 20:24:26 +0800

    add demo code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,39 @@
 #include "../lib/mylib/adder.h"
 #include <stdio.h>
+#include <GLFW/glfw3.h>
+#include <cstdlib>
 
-int main(){
-    printf("2+4 = %d\n", jamesMath::add(2, 4));
-    return 0;
+int main(int argc, char *argv[])
+{
+    printf("this demo opens a blank window\n");
+    GLFWwindow *window;
+
+    if (!glfwInit())
+    {
+        fprintf(stderr, "Failed to initialize GLFW\n");
+        exit(EXIT_FAILURE);
+    }
+
+    window = glfwCreateWindow(300, 300, "Gears", NULL, NULL);
+    if (!window)
+    {
+        fprintf(stderr, "Failed to open GLFW window\n");
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+    // Main loop
+    while (!glfwWindowShouldClose(window))
+    {
+
+        // Swap buffers
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // Terminate GLFW
+    glfwTerminate();
+
+    // Exit program
+    exit(EXIT_SUCCESS);
 }
