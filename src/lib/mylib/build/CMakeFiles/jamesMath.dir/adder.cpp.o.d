1cc00d42119a1b629cce46ec50b3dca8e019bc7b
James-Lu-none <jamesabcde277@gmail.com>
2024-04-09 13:23:19 +0800

    add .gitignore

--- src/lib/mylib/build/CMakeFiles/jamesMath.dir/adder.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/jamesMath.dir/adder.cpp.o: \
- /home/james/projects/cProject/cmakePractice/src/lib/mylib/adder.cpp \
- /usr/include/stdc-predef.h \
- /home/james/projects/cProject/cmakePractice/src/lib/mylib/adder.h
